64285c58b226270e46b6220e9cdb06970ffa32fb
onebitbool <onebitbool@gmail.com>
2022-07-27 17:15:10 +0800

    Finish hw03 q6

--- HWs/hw03/hw03.py
+++ HWs/hw03/hw03.py
@@ -204,4 +204,10 @@ def move_stack(n, start, end):
     Move the top disk from rod 1 to rod 3
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
-    "*** YOUR CODE HERE ***"
+    if n == 1:
+        print_move(start, end)
+    else:
+        free_stack = 6 - start - end
+        move_stack(n - 1, start, free_stack)
+        move_stack(1, start, end)
+        move_stack(n - 1, free_stack, end)
